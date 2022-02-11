016d66e5fde5fd6f00b2eb3ebe85216e6bbedcb7
mysteryon88 <mysteryon88@yandex.ru>
2022-02-11 21:11:31 +0300

    update
    1. encryption
    2. decryption
    3. working with files
    4. minor changes

--- RSA.cpp
+++ RSA.cpp
@@ -1,8 +1,12 @@
 ﻿#include "RSA.h"
 
+#include <iterator>
+
 RSA::RSA() {
-	privat_key = 0ull;
-	public_key = 0ull;
+	privat_key.exp = 0ull;
+	privat_key.modulus = 0ull;
+	public_key.exp = 0ull;
+	public_key.modulus = 0ull;
 }
 
 RSA::~RSA() {}
@@ -14,33 +18,52 @@ void RSA::GenerateKey() {
 	std::mt19937 mersenne(rd()); // initialize the Mersenne Twister with a random starting number
 	//generate p
 	do {
-		p = 2000ull + mersenne() % 40000;
+		p = 500ull + mersenne() % 2000;
 		if (IsPrime(p)) break;
 	} while (true);
 	//generate q
 	do {
-		q = 2000ull + mersenne() % 40000;
-		if (IsPrime(q)) break;
+		q = 500ull + mersenne() % 2000;
+		if (q != p && IsPrime(q)) break;
 	} while (true);
+
 	//p = 3557;
 	//q = 2579;
 	//Their product n = p*q is calculated, which is called the modulus.
 	modulus = p * q;
+	public_key.modulus = modulus;
+	privat_key.modulus = modulus;
 
 	//The value of the Euler function from the number n is calculated: φ(n) = (p−1)⋅(q−1)
 	euler = (p - 1) * (q - 1);
 
 	//An integer e ( 1 < e < φ(n) ) is chosen, coprime with the value of the Euler function (t)
 	//open exhibitor
-	open_exp = CalculateE(euler);			  //e
+	public_key.exp = CalculateE(euler);			        //e
 
-	secret_exp = CalculateD(open_exp, euler); //d
+	privat_key.exp = CalculateD(public_key.exp, euler); //d
+
+	//writing to file
+	std::ofstream public_file;
+	public_file.open("public.key");
+	if (public_file.is_open()) 
+		public_file << public_key.exp << " " << public_key.modulus;
+	public_file.close();
+
+	//writing to file
+	std::ofstream privat_file;
+	privat_file.open("privat.key");
+	if (privat_file.is_open())
+		privat_file << privat_key.exp << " " << privat_key.modulus;
+	privat_file.close();
 
 	//Пара {e, n} публикуется в качестве открытого ключа RSA
-	std::cout << "\nRSA public key is (n = " << modulus << ", e = " << open_exp << ")" << std::endl;
+	std::cout << "\nRSA public key is (n = " << public_key.modulus << ", e = " << public_key.exp << ")" << std::endl;
 
 	//Пара {d, n} играет роль закрытого ключа RSA и держится в секрете
-	std::cout << "RSA private key is (n = " << modulus << ", d = " << secret_exp << ")" << std::endl;
+	std::cout << "RSA private key is (n = " << privat_key.modulus << ", d = " << privat_key.exp << ")" << std::endl;
+
+	Encrypt("23");
 }
 
 //miller-rabin test (start)
@@ -56,14 +79,14 @@ bool RSA::IsPrime(uint64_t n, unsigned short iter) {
 	}
 	for (unsigned short i = 0; i < iter; i++) {
 		int a = 2 + rand() % (n - 3);
-		if (check_composite(n, a, d, s))
+		if (CheckComposite(n, a, d, s))
 			return false;
 	}
 	return true;
 }
 
-bool RSA::check_composite(uint64_t n, uint64_t a, uint64_t d, int s) {
-	uint64_t x = binpower(a, d, n);
+bool RSA::CheckComposite(uint64_t n, uint64_t a, uint64_t d, int s) {
+	uint64_t x = Binpower(a, d, n);
 	if (x == 1 || x == n - 1)
 		return false;
 	for (int r = 1; r < s; r++) {
@@ -74,7 +97,8 @@ bool RSA::check_composite(uint64_t n, uint64_t a, uint64_t d, int s) {
 	return true;
 }
 
-uint64_t RSA::binpower(uint64_t base, uint64_t e, uint64_t mod) {
+//fast exponentiation
+uint64_t RSA::Binpower(uint64_t base, uint64_t e, uint64_t mod) {
 	uint64_t result = 1;
 	base %= mod;
 	while (e) {
@@ -134,3 +158,63 @@ int64_t RSA::gcdex(int64_t a, int64_t b, int64_t& x, int64_t& y) {
 	return d;
 }
 //Calculate secret exponent (end)
+
+void RSA::Encrypt(std::string path)
+{	
+	std::string data_hash = "HEllo WOrld!";
+	/*
+	std::ifstream hash(path); // open file with hash
+	if (hash.is_open()) {
+		//while (getline(hash, data_hash)); 
+		getline(hash, data_hash);
+	}
+	hash.close();
+	*/
+	
+	std::vector<uint64_t> encrypt_hash(data_hash.length());
+	
+	for(size_t i = 0; i < data_hash.length(); ++i)
+		encrypt_hash[i] = Binpower((uint16_t)data_hash[i], public_key.exp, public_key.modulus);
+
+	std::ofstream encrypted;
+	encrypted.open("signed file.enc");
+	if (encrypted.is_open())
+		std::copy(encrypt_hash.begin(), encrypt_hash.end(), std::ostream_iterator<uint64_t>(encrypted, " "));
+	encrypted.close();
+
+	//-----
+
+	//std::copy(encrypt_hash.begin(), encrypt_hash.end(), std::ostream_iterator<uint64_t>(std::cout, " "));
+	//std::cout << "\n";
+
+	Decipher("signed file.enc");
+}
+
+void RSA::Decipher(std::string path) {
+	//path = signed file.enc
+	std::ifstream encrypted(path); // open file with encrypted hash
+	std::vector<uint64_t> encrypted_hash(100); //-----------------------------------не забыть про размер хэша
+	if (encrypted.is_open()) {
+		size_t i = 0;
+		while (!encrypted.eof()) {
+			encrypted >> encrypted_hash[i];
+			i++;
+		}
+	}
+	encrypted.close();
+
+	//hash decryption
+	std::vector<uint8_t> decrypted_hash(encrypted_hash.size());
+
+	for (size_t i = 0; i < encrypted_hash.size(); ++i)
+		decrypted_hash[i] = Binpower(encrypted_hash[i], privat_key.exp, privat_key.modulus);
+
+	//write hash to file for comparison
+	std::ofstream decrypted;
+	decrypted.open("decrypted.hash");
+	if (decrypted.is_open())
+		std::copy(decrypted_hash.begin(), decrypted_hash.end(), std::ostream_iterator<uint8_t>(decrypted));
+	decrypted.close();
+
+	std::copy(decrypted_hash.begin(), decrypted_hash.end(), std::ostream_iterator<uint8_t>(std::cout));
+}
--- RSA.h
+++ RSA.h
@@ -1,27 +1,35 @@
 #pragma once
+#include <iostream>
+#include <fstream>
 #include <cstdint>
 #include <random>
-#include <iostream>
+#include <string>
+
+typedef struct {
+    uint64_t exp;
+    uint64_t modulus;
+} Key;
 
 class RSA
 {
 private:
-    uint64_t privat_key;
-    uint64_t public_key;
+    Key privat_key;
+    Key public_key;
 
     bool IsPrime(uint64_t n, unsigned short iter = 5);
-    bool check_composite(uint64_t n, uint64_t a, uint64_t d, int s);
-    uint64_t binpower(uint64_t base, uint64_t e, uint64_t mod);
+    bool CheckComposite(uint64_t n, uint64_t a, uint64_t d, int s);
+    uint64_t Binpower(uint64_t base, uint64_t e, uint64_t mod);
 
     uint64_t CalculateE(uint64_t t);
     uint64_t GreatestCommonDivisor(uint64_t e, uint64_t t);
 
     int64_t CalculateD(int64_t a, int64_t m);
     int64_t gcdex(int64_t a, int64_t b, int64_t& x, int64_t& y);
+
 public:
     RSA();
     ~RSA();
     void GenerateKey();
-        
+    void Encrypt(std::string path);
+    void Decipher(std::string path);
 };
-
